c69ef6178d4ccbdbb3a7464d79b8248839fb9d9b
unknown <efrenmedina@live.com>
2019-03-07 10:03:38 -0500

    Project Start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
+import { ProductsComponent } from './products/products.component';
+import { DeleteComponent } from './delete/delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomepageComponent,
+    AboutComponent,
+    ProductsComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,

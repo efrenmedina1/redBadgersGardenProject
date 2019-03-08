f83b4294f091853e995905e20020abf6f3fc8ae7
unknown <efrenmedina@live.com>
2019-03-08 09:27:22 -0500

    Delete Update

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from './products.service';
+import { del } from 'selenium-webdriver/http';
 
 @Component({
   selector: 'app-products',
@@ -8,9 +9,16 @@ import { ProductService } from './products.service';
 })
 export class ProductsComponent implements OnInit {
 public products = {};
+public admin = "";
+public role = "";
+
   constructor(private _productService: ProductService) { }
 
+ 
+
   ngOnInit() {
+
+
     this._productService.getProducts()
       .subscribe(data => 
         // console.log(data)
@@ -19,6 +27,31 @@ public products = {};
         );
         
       
+  }
+  
+  deleteProduct(e) {
+    e.preventDefault();
+    console.log(e);
+    var delID = e.target.elements[0].id;
+    var delUrl = `https://efa-gardenapp-backend.herokuapp.com/api/product/${delID}`;
+    var token = sessionStorage.getItem('token');
+    console.log(token);
+
+    fetch(delUrl, {
+      method: 'DELETE',
+      headers: new Headers({
+        Authorization: token
+      })
+    }).then(response => response.json())
+      .then(json => { console.log(json)})
+
+      .then(refresh =>
+        this._productService.getProducts()
+      .subscribe(data => 
+        // console.log(data)
+        this.products = data
+        // console.log(this.products)
+        ) )
   }
 
 }
